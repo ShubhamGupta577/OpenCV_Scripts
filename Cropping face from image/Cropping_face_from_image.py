16130a6489cd3a3430becf05f152ce045b8e74df
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-07 01:29:29 +0530

    Create Cropping_face_from_image.py

---
+++ Cropping face from image/Cropping_face_from_image.py
@@ -0,0 +1,33 @@
+import cv2
+
+# Taking the path for input image
+path=input("Enter the path of the image: ")
+img = cv2.imread(path)
+#Resizing the image
+img=cv2.resize(img,(600,600))
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('/content/haarcascade_frontalface_alt2.xml')
+
+# Detecting the faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+i=0
+# Draw rectangle around the faces and crop the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    faces = img[y:y + h, x:x + w]
+    cv2.imshow('face',faces)
+    #Naming the file
+    desti='face'+str(i)+'.jpg'
+    cv2.imwrite(desti, faces)
+    i+=1
+
+	
+# Display the output
+cv2.imwrite('detcted.jpg', img)
+cv2.imshow('Output',img)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
