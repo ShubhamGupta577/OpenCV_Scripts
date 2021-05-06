9c5d05d8ac043685490f693921c37afe0f0d12dc
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-07 01:31:24 +0530

    Update Cropping_face_from_image.py

--- Cropping face from image/Cropping_face_from_image.py
+++ Cropping face from image/Cropping_face_from_image.py
@@ -8,8 +8,9 @@ img=cv2.resize(img,(600,600))
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
+# Taking the path of cascade file
+path=input("Enter the path of the haarcascade_frontalface.xml file: ")
+face_cascade = cv2.CascadeClassifier(path)
 
 # Detecting the faces
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
