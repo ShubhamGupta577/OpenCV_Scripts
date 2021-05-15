bcce68c5f6fbf430961f8d3538f68e10e3e74582
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-15 23:39:39 +0530

    Update Shape_Detection.py

--- Shape Detection/Shape_Detection.py
+++ Shape Detection/Shape_Detection.py
@@ -2,5 +2,7 @@ import cv2
 path=input('Enter the path of image : ')
 img=cv2.imread(path)
 img=cv2.resize(img,(600,600))
-
+gray=cv2.cvtColor(img,cv2.COLORBGR2GRAY)
+blur=cv2.Gaussianblur(gray,(5,5),0)
+_,thresh=cv2.threshold(gray)
 
