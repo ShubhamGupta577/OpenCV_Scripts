9de8f00260b23171d64bc95a256d805c62c7188c
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-15 23:41:10 +0530

    Update Shape_Detection.py

--- Shape Detection/Shape_Detection.py
+++ Shape Detection/Shape_Detection.py
@@ -1,7 +1,10 @@
 import cv2
+#Taking input for query image
 path=input('Enter the path of image : ')
 img=cv2.imread(path)
+#Resizing the image
 img=cv2.resize(img,(600,600))
+#Blurring the image and converting image into binary
 gray=cv2.cvtColor(img,cv2.COLORBGR2GRAY)
 blur=cv2.Gaussianblur(gray,(5,5),0)
 _,thresh=cv2.threshold(gray)
