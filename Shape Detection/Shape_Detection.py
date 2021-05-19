a0b9f64826b847a2109e893efa7db9dd49296d88
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-05-19 23:41:45 +0530

    Update Shape_Detection.py

--- Shape Detection/Shape_Detection.py
+++ Shape Detection/Shape_Detection.py
@@ -1,11 +1,66 @@
 import cv2
-#Taking input for query image
-path=input('Enter the path of image : ')
-img=cv2.imread(path)
-#Resizing the image
-img=cv2.resize(img,(600,600))
-#Blurring the image and converting image into binary
-gray=cv2.cvtColor(img,cv2.COLORBGR2GRAY)
-blur=cv2.Gaussianblur(gray,(5,5),0)
-_,thresh=cv2.threshold(gray)
+import imutils
+from google.colab.patches import cv2_imshow
+def detect(c):
+    # initialize the shape name and approximate the contour
+    shape = "unidentified"
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+    # if the shape is a triangle, it will have 3 vertices
+    if len(approx) == 3:
+        shape = "triangle"
+    # if the shape has 4 vertices, it is either a square or
+    # a rectangle
+    elif len(approx) == 4:
+        # compute the bounding box of the contour and use the
+        # bounding box to compute the aspect ratio
+        (x, y, w, h) = cv2.boundingRect(approx)
+        ar = w / float(h)
+        # a square will have an aspect ratio that is approximately
+        # equal to one, otherwise, the shape is a rectangle
+        shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+    # if the shape is a pentagon, it will have 5 vertices
+    elif len(approx) == 5:
+        shape = "pentagon"
+    # otherwise, we assume the shape is a circle
+    else:
+        shape = "circle"
+    # return the name of the shape
+    return shape
 
+# load the image and resize it to a smaller factor so that
+# the shapes can be approximated better
+path=input("Enter the path of the image: ")
+image = cv2.imread(path)
+resized = imutils.resize(image, width=300)
+ratio = image.shape[0] / float(resized.shape[0])
+# convert the resized image to grayscale, blur it slightly,
+# and threshold it
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+ret,thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+cv2_imshow(thresh)
+# find contours in the thresholded image and initialize the
+# shape detector
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour, then detect the name of the
+	# shape using only the contour
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]) * ratio)
+	cY = int((M["m01"] / M["m00"]) * ratio)
+	shape = detect(c)
+	# multiply the contour (x, y)-coordinates by the resize ratio,
+	# then draw the contours and the name of the shape on the image
+	c = c.astype("float")
+	c *= ratio
+	c = c.astype("int")
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+		0.5, (255, 255, 255), 2)
+	# show the output image
+	cv2_imshow(image)
